5636721ddaf634354e2e74e7b69dbd0521ae69e2
AdrienW86 <adrien_weiss@outlook.fr>
2025-06-19 09:44:50 +0200

    mise en ligne

--- app/anti-mousse/page.js
+++ app/anti-mousse/page.js
@@ -4,8 +4,8 @@ import Mousse from '@/components/Mousse/Mousse'
 import styles from '@/app/page.module.css'
 
 export const metadata = {
-  title: 'Démoussage de toiture professionnel avec Toiture 66 - Pyrénées-Orientales',
-  description: 'Toiture 66 à Perpignan vous propose un démoussage de toiture professionnel pour éliminer mousses, lichens et algues. Nettoyage et traitement anti-mousse efficaces dans les Pyrénées-Orientales.',
+  title: 'Démoussage de toiture professionnel avec Entreprise Gurtner - Loire-Atlantique',
+  description: 'Entreprise Gurtner à Nantes vous propose un démoussage de toiture professionnel pour éliminer mousses, lichens et algues. Nettoyage et traitement anti-mousse efficaces dans la Loire-Atlantique.',
 };
 
 export default function page() {
