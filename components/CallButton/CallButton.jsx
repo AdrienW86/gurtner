3477e93467fd7b60a367140b2854a1bb56afcba8
AdrienW86 <adrien_weiss@outlook.fr>
2025-06-19 07:52:26 +0200

    mise en ligne

--- components/CallButton/CallButton.jsx
+++ components/CallButton/CallButton.jsx
@@ -3,7 +3,7 @@ import styles from './button.module.css'
 
 export default function CallButton() {
     return (
-        <a href="tel:+33757516414" className={styles.link}>
+        <a href="tel:+33628286967" className={styles.link}>
             <button className={styles.btn}>
             📞 Appeler
             </button>
