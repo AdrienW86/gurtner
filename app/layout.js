ff92fa3e674b140129b83f297eb81aae01978a4e
AdrienW86 <adrien_weiss@outlook.fr>
2025-06-19 18:07:30 +0200

    misen en ligne

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,12 @@ const oswald = Oswald({
   display: 'swap',
 });
 
+export const metadata = {
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Banner from '@/components/Banner/Banner'
 import Equipment from '@/components/Equipment/Equipment'
 import Description from '@/components/Description/Description'
-import PhoneButton from '@/components/PhoneButton/PhoneButton'
 import styles from './page.module.css'
 
 export const metadata = {
